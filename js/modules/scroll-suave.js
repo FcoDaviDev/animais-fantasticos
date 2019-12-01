2f227a440ff4d63e2d92f0b381baaff3936a712d
FcoDaviDev <davigalmeida@hotmail.com>
2019-12-01 10:05:59 -0300

    primeiro commit

---
+++ js/modules/scroll-suave.js
@@ -0,0 +1,25 @@
+export default function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    // forma alternativa
+    // const topoSection = section.offsetTop;
+  // window.scrollTo({
+  //     top: topoSection,
+  //     behavior: 'smooth'
+  // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
